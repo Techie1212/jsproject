766ec934640cb26d1f89c000f470f25a9b95c09c
Ajay Katla <86738677+Techie1212@users.noreply.github.com>
2023-09-19 13:46:55 +0530

    done with changes in html through js

--- demo.js
+++ demo.js
@@ -1,1 +1,6 @@
-console.log("I love git")
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.style.borderBottom = "solid 3px #000";
+headerTitle.textContent = "Hello there !"
+var Title = document.getElementById('main');
+Title.style.color = "green";
