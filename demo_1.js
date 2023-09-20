f72213d579cfa3957f90f2f38350b9f5c5a21172
Ajay Katla <86738677+Techie1212@users.noreply.github.com>
2023-09-20 11:44:53 +0530

    have changed the content

--- demo_1.js
+++ demo_1.js
@@ -3,14 +3,11 @@ console.log(headerTitle);
 headerTitle.style.borderBottom = "solid 3px #000";
 headerTitle.textContent = "Hello there !"
 var li = document.getElementsByTagName('li');
-li[2].style.backgroundColor = "green";
+li[1].style.backgroundColor = "green";
+li[2].style.display = "none"
 var items = document.querySelector('li')
 for(i = 0; i < li.length; i++){
      li[i].style.textTransform = 'uppercase';
 }
-var list = document.getElementsByClassName('list-group-item');
-   list[1].style.backgroundColor = "#ccc";
-var list = document.getElementsByTagName('li');
-    list[0].style.backgroundColor = "blue";
 var input = document.querySelector('input');
 input.value = "Hello world";
