80ea258f3257dc8adff3d06f0c70eed5876e7adf
Ajay Katla <86738677+Techie1212@users.noreply.github.com>
2023-09-20 11:12:02 +0530

    updated html and js

--- demo.js
+++ demo.js
@@ -2,5 +2,9 @@ var headerTitle = document.getElementById('header-title');
 console.log(headerTitle);
 headerTitle.style.borderBottom = "solid 3px #000";
 headerTitle.textContent = "Hello there !"
-var Title = document.getElementById('main');
-Title.style.color = "green";
+var li = document.getElementsByTagName('li');
+li[2].style.backgroundColor = "green";
+var items = document.querySelector('li')
+for(i = 0; i < li.length; i++){
+     li[i].style.textTransform = 'uppercase';
+}
