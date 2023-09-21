faa285108fee20fdb9639bf6516dc4f8e3e29529
Ajay Katla <86738677+Techie1212@users.noreply.github.com>
2023-09-21 12:31:58 +0530

    updated DOM in js

--- demo_1.js
+++ demo_1.js
@@ -11,3 +11,22 @@ for(i = 0; i < li.length; i++){
 }
 var input = document.querySelector('input');
 input.value = "Hello world";
+var listItems = document.querySelector('#items');
+console.log(listItems.firstChild);
+console.log(listItems.lastChild);
+console.log(listItems.previousSibling);
+console.log(listItems.previousElementSibling);
+console.log(listItems.children);
+console.log(listItems.firstElementChild);
+console.log(listItems.lastElementChild);
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute('title', 'hello world');
+var newDivText = document.createTextNode('Hello world');
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1= document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
